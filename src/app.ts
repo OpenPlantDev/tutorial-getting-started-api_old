98efe8302e193a6cf534ecf3319b629c1dea19f9
Dan Nichols <Dan.Nichols@bentley.com>
2019-02-15 18:48:58 -0600

    added lint to build fixed lint issues

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,21 @@
 
 class MathUtils {
 
-  Add = (a: number, b: number) : number => {
+  public add = (a: number, b: number): number => {
     return a + b;
   }
 
-  Multiply = (a: number, b: number) : number => {
+  public multiply = (a: number, b: number): number => {
     return a + b;
   }
 }
 
-
 let mathUtils = new MathUtils();
 
-let n1 = 3;
+let n1 = 2;
 let n2 = 2;
-let sum = mathUtils.Add(n1, n2);
+let sum = mathUtils.add(n1, n2);
 console.log(`sum of ${n1} and ${n2} = ${sum}`);
 
-let product = mathUtils.Multiply(n1, n2);
+let product = mathUtils.multiply(n1, n2);
 console.log(`product of ${n1} and ${n2} = ${product}`);
