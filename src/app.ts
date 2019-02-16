07eca4b3e7defb5a3068fe4a21dafdef80fcbe27
Dan Nichols <Dan.Nichols@bentley.com>
2019-02-15 22:24:55 -0600

    Added unit test using mocha and chai

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,11 @@
+import {MathUtils} from "./utilities/mathUtils";
 
-class MathUtils {
+const mathUtils = new MathUtils();
 
-  public add = (a: number, b: number): number => {
-    return a + b;
-  }
-
-  public multiply = (a: number, b: number): number => {
-    return a + b;
-  }
-}
-
-let mathUtils = new MathUtils();
-
-let n1 = 2;
-let n2 = 2;
-let sum = mathUtils.add(n1, n2);
+const n1 = 2;
+const n2 = 2;
+const sum = mathUtils.add(n1, n2);
 console.log(`sum of ${n1} and ${n2} = ${sum}`);
 
-let product = mathUtils.multiply(n1, n2);
+const product = mathUtils.multiply(n1, n2);
 console.log(`product of ${n1} and ${n2} = ${product}`);
---
+++ src/utilities/mathUtils.ts
@@ -0,0 +1,10 @@
+export class MathUtils {
+
+  public add = (a: number, b: number): number => {
+    return a + b;
+  }
+
+  public multiply = (a: number, b: number): number => {
+    return a * b;
+  }
+}
